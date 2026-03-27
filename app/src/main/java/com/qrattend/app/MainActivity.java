a972175b09786ae418c7b83304ad304ced4a49ae
coder456789 <piyushpawar.777999@gmail.com>
2026-03-27 13:20:30 +0530

    confirmation that firebase has been connected to the project

--- app/src/main/java/com/qrattend/app/MainActivity.java
+++ app/src/main/java/com/qrattend/app/MainActivity.java
@@ -1,24 +1,20 @@
 package com.qrattend.app;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_main); // This line links to your UI
+
+        // Initialize Firebase
+        FirebaseApp.initializeApp(this);
+
+        // If the app reaches this line without crashing, you win!
+        Toast.makeText(this, "Firebase Connected Successfully!", Toast.LENGTH_LONG).show();
     }
 }
